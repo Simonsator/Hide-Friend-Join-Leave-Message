787558db5f94a18abf4d132ab714f7142d02e39d
Simonsator <00pflaume@gmail.com>
2018-02-01 13:51:36 +0100

    Fixed problems with the next update of PAF

--- src/main/java/de/simonsator/partyandfriends/hfjlm/HFJLMMain.java
+++ src/main/java/de/simonsator/partyandfriends/hfjlm/HFJLMMain.java
@@ -1,16 +1,17 @@
 package de.simonsator.partyandfriends.hfjlm;
 
+import de.simonsator.partyandfriends.api.PAFExtension;
 import de.simonsator.partyandfriends.api.events.OnlineStatusChangedMessageEvent;
+import de.simonsator.partyandfriends.api.pafplayers.OnlinePAFPlayer;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.plugin.Listener;
-import net.md_5.bungee.api.plugin.Plugin;
 import net.md_5.bungee.event.EventHandler;
 
 /**
  * @author simonbrungs
  * @version 1.0.0 29.12.16
  */
-public class HFJLMMain extends Plugin implements Listener {
+public class HFJLMMain extends PAFExtension implements Listener {
 	private final String PERMISSION = "partyandfriends.showmessage";
 
 	@Override
@@ -20,6 +21,10 @@ public class HFJLMMain extends Plugin implements Listener {
 
 	@EventHandler
 	public void onOnlineStatusChanged(OnlineStatusChangedMessageEvent pEvent) {
+		if (!(pEvent.getPlayer() instanceof OnlinePAFPlayer)) {
+			pEvent.setCancelled(true);
+			return;
+		}
 		if (!pEvent.getPlayer().hasPermission(PERMISSION))
 			pEvent.setCancelled(true);
 	}
